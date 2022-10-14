5fd821eb8f8ea384112665b9239c762dc9f20a2c
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2022-10-14 09:46:48 +0200

    Add project files.

---
+++ D18-ovn-1.3-gitrepo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D18_ovn_1._3_gitrepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
